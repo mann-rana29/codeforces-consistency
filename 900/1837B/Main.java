ae206bc937bdcca38a5fd130e36f79d58aa620bb
the25thjin <mannr075@gmail.com>
2026-01-08 23:53:48 +0530

    Comparison String

--- 900/1837B/Main.java
+++ 900/1837B/Main.java
@@ -6,29 +6,28 @@ public class Main {
     static FastScanner fs = new FastScanner();
     static StringBuilder out = new StringBuilder();
 
-    static void A() throws Exception {
+    static void solve() throws Exception {
+        // write solution here
         int n = fs.nextInt();
         String s = fs.next();
+        int maxLen = 1;
+        int len = 1;
+        for(int i = 1; i < n; i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                len++;
+            }else{
+                len = 1;
+            }
 
-        if(s.contains("2026")){
-            out.append(0).append("\n");
-            return;
+            maxLen = Math.max(maxLen,len);
         }
-        else if(!s.contains("2025")){
-            out.append(0).append("\n");
-            return;
-        }else{
-            out.append(1).append("\n");
-            return;
-        }
-
+        out.append(maxLen+1).append("\n");
     }
-
 
     public static void main(String[] args) throws Exception {
         int t = fs.nextInt();
         while (t-- > 0) {
-            A();
+            solve();
         }
         System.out.print(out.toString());
     }
